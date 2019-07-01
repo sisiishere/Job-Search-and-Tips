b4a8ae524a07adaf6ecf01206467a5edbe06c3f3
Abhishek Maan <AbhishekMaan1997@gmail.com>
2019-07-01 11:02:59 -0400

    Working on youtube data call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,4 +3,26 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
 });
 
+$(function(){
+    $("#submit").on('click', function(e) {
+        e.preventDefault();
+        //preparing the kind of information that is to be requested
+        var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#keyword").val()).replace(/%20/g, "+"),
+            maxResults: 2,
+            order: "viewCount",
+        });
+        request.execute(function(response){
+            console.log(response);
+        });
+    });
+});
 
+function init(){
+    gapi.client.setApiKey("AIzaSyCa6Ax79dsyv4aSME_i7u3VKv7ApWTbAe4");
+    gapi.client.load("youtube", "v3", function(){
+        //Youtube API is ready
+    });
+};
